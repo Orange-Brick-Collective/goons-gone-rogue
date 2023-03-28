c66c0d4645016802bd20082b883df80ea96893e3
Kodi022 <kodi01012@gmail.com>
2023-03-27 22:41:03 -0700

    work on randomly generated, console 'gen' to see

--- code/game/Level.cs
+++ code/game/Level.cs
@@ -4,10 +4,92 @@ using System.Collections.Generic;
 namespace GGame;
 
 public class Level {
-    int depth = 0;
-    List<Tile> tiles;
+    public int depth = 0;
+    public int wallType = 0;
+    public List<List<Tile>> tiles;
 }
 
 public class Tile : ModelEntity {
-    // item tiletype (enemy, chest, idk)
+    public bool[] directions = {false, false, false, false}; // NESW
+    public Transform northPoint, eastPoint, southPoint, westPoint; // NESW
+    public ModelEntity northWall, eastWall, southWall, westWall; // NESW
+    public AnimatedEntity tileEvent;
+
+    public virtual string StreetModel {get; set;}
+    public virtual string WallModel {get; set;}
+
+    public Tile() {}
+    public Tile(Vector3 position, string wallModel) {
+        Tags.Add("tile");
+
+        Position = position;
+
+        SetModel(StreetModel);
+
+        // if (directions[0]) {
+        //     northPoint = Model.GetAttachment("northpoint").Value;
+        //     northWall = new();
+        //     northWall.SetModel(wallModel);
+        //     northWall.Transform = northPoint;
+        // }
+        // if (directions[1]) {
+        //     eastPoint = Model.GetAttachment("eastpoint").Value;
+        //     eastWall = new();
+        //     eastWall.SetModel(wallModel);
+        //     eastWall.Transform = eastPoint; 
+        // }
+        // if (directions[2]) {
+        //     southPoint = Model.GetAttachment("southpoint").Value;
+        //     southWall = new();
+        //     southWall.SetModel(wallModel);
+        //     southWall.Transform = southPoint;
+        // }
+        // if (directions[3]) {
+        //     westPoint = Model.GetAttachment("westpoint").Value;
+        //     westWall = new();
+        //     westWall.SetModel(wallModel);
+        //     westWall.Transform = westPoint;
+        // }
+    }
+
+	protected override void OnDestroy() {
+        northWall?.Delete();
+        eastWall?.Delete();
+        southWall?.Delete();
+        westWall?.Delete();
+        tileEvent?.Delete();
+		base.OnDestroy();
+	}
+}
+
+public class TileStraight : Tile {
+    public new bool[] directions = {true, false, true, false};
+    public override string StreetModel {get; set;} = "models/map-straight.vmdl";
+
+    public TileStraight() {}
+    public TileStraight(Vector3 position, string wallModel) : base(position, wallModel) {}
+}
+
+public class TileCorner : Tile {
+    public new bool[] directions = {true, true, false, false};
+    public override string StreetModel {get; set;} = "models/map-corner.vmdl";
+
+    public TileCorner() {}
+    public TileCorner(Vector3 position, string wallModel) : base(position, wallModel) {}
+}
+
+public class TileT : Tile {
+    public new bool[] directions = {true, false, true, true};
+    public override string StreetModel {get; set;} = "models/map-t-junction.vmdl";
+
+    public TileT() {}
+    public TileT(Vector3 position, string wallModel) : base(position, wallModel) {}
+}
+
+public class TileX : Tile {
+    public new bool[] directions = {true, true, true, true};
+    public override string StreetModel {get; set;} = "models/map-x-junction.vmdl";
+
+    public TileX() {}
+    public TileX(Vector3 position, string wallModel) : base(position, wallModel) {}
 }
--- code/game/WorldManager.cs
+++ code/game/WorldManager.cs
@@ -13,8 +13,53 @@ public class WorldManager {
         Cur = this;
     }
 
-    public void Generatelevel(string[] tiles, int depth) {
+    [ConCmd.Server("gen")]
+    public static void GenerateLevelCMD() {
+        Cur.GenerateLevel(8, 6, 0);
+    }
 
+    public void GenerateLevel(int length, int width, int depth) {
+        Log.Info("Generating");
+
+        foreach (Entity ent in Entity.All) {
+            if (ent.Tags.Has("tile")) ent.Delete();
+        }
+
+        Level lvl = new() {
+            depth = depth,
+            wallType = Random.Shared.Int(0, 3),
+            tiles = new(),
+        };
+
+        Vector2 startPoint = new(0, Random.Shared.Int(0, width));
+        Vector2 endPoint = new(length, Random.Shared.Int(0, width));
+
+        for (int l = 0; l < length; l++) {
+            lvl.tiles.Add(new List<Tile>());
+            for (int w = 0; w < width; w++) {
+
+                switch(Random.Shared.Int(0, 3)) {
+                    case 0: {
+                        lvl.tiles[l].Add(new TileStraight(new Vector3(l * 512, w * 512), ""));
+                        break;
+                    }
+                    case 1: {
+                        lvl.tiles[l].Add(new TileCorner(new Vector3(l * 512, w * 512), ""));
+                        break;
+                    }
+                    case 2: {
+                        lvl.tiles[l].Add(new TileT(new Vector3(l * 512, w * 512), ""));
+                        break;
+                    }
+                    case 3: {
+                        lvl.tiles[l].Add(new TileX(new Vector3(l * 512, w * 512), ""));
+                        break;
+                    }
+                }
+            }
+        }
+
+        currentLevel = lvl;
     }
 }
 
